1591ccce7bf1f8476bce21e64f903e6b025803a8
Gaurav Appa <gauravappagga@gmail.com>
2022-01-31 05:00:58 +0530

    initial commit

---
+++ src/main/java/com/java/alogrithms/App.java
@@ -0,0 +1,13 @@
+package com.java.alogrithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
